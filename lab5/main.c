960d8f994e2f300c0280b9951e79e5cd3a7b45d0
陳民祥 <119960255+b1129055@users.noreply.github.com>
2023-04-13 14:33:31 +0800

    Update main.c

--- lab5/main.c
+++ lab5/main.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+void multi(int i, int j) {
+	printf("%d * %d = %d\t", i, j, i * j);
+	if(i * j == 81) {
+		return;
+	}else if (j == 9) {
+		i++;
+		j = 0;
+		printf("\n");
+	}
+	j++;
+	multi(i, j);
+}
+
+int main() {
+	multi(1,1);
+	return 0;
+}
